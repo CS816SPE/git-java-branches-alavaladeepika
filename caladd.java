638a189112651a0411a146aca5fc299f687c5fba
alavaladeepika <99alavaladeepika@gmail.com>
2019-01-17 10:51:30 +0530

    MULTIPLY Operation

--- caladd.java
+++ caladd.java
@@ -11,7 +11,8 @@ public class caladd {
         do {
             System.out.print("1. ADD TWO Numbers\n");
             System.out.print("2. SUBTRACT TWO Numbers\n");
-            System.out.print("3. Exit\n\n");
+            System.out.print("3. MULTIPLY TWO Numbers\n");
+            System.out.print("4. Exit\n\n");
             System.out.print("Enter your choice : ");
             choice = scan.next().charAt(0);
 
@@ -33,6 +34,14 @@ public class caladd {
                     break;
 
                 case '3':
+                    System.out.print("Enter two numbers: ");
+                    a = scan.nextFloat();
+                    b = scan.nextFloat();
+                    res = a * b;
+                    System.out.print("Result = " + res);
+                    break;
+
+                case '4':
                     System.exit(0);
                     break;
 
@@ -43,6 +52,6 @@ public class caladd {
 
             System.out.print("\n----------------------------------------\n");
         
-        }while(choice != 3);
+        }while(choice != 4);
     }
 }
